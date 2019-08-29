68a345e6e004dabc7dce58091d8377455ab68c2a
Sergey Sukhanov <sssuhanov@gmail.com>
2019-08-29 12:15:14 +0300

    with defs v0.02

--- main.py
+++ main.py
@@ -4,36 +4,54 @@ import json
 import os
 import time
 
-session = requests.session()
 
-# Добавляем к сессии прокси
-session.proxies = {}
-session.proxies['http'] = 'socks5h://localhost:9050'
-session.proxies['https'] = 'socks5h://localhost:9050'
+def do_session():
+    session = requests.session()
 
-# Подменяем headers
-headers = {}
-headers['User-agent'] = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'
+    # Добавляем к сессии прокси
+    session.proxies = {}
+    session.proxies['http'] = 'socks5h://localhost:9050'
+    session.proxies['https'] = 'socks5h://localhost:9050'
 
-date_start = '19370101'
-date_end = '20190829'
+    return session
 
-URL=f'https://api-ak.wunderground.com/api/75e91f5c866f39a2/history_{date_start}{date_end}/lang:EN/units:english/bestfct:1/v:2.0/q/UUWW.json'
-parforget = {'showObs' : '0',
-            'ttl' : '120'}
+def get_params_for_req():
+    params = {}
+    # Подменяем headers
+    params['headers'] = {}
+    params['headers']['User-agent'] = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'
 
-try:
-    # reqObj = requests.get("https://www.wunderground.com/history/monthly/ru/moscow/UUWW/date/2013-8", timeout=10)
-    r = session.get(URL, headers=headers, params=parforget)
-except HTTPError as e:
-    print(e)
-else:
-    pass
+    date_start = '19370101'
+    date_end = '20190829'
 
-# Сохраняем json
-dir_out = 'out'
-if not os.path.exists(dir_out):
-    os.makedirs(dir_out)
+    params['URL']=f'https://api-ak.wunderground.com/api/75e91f5c866f39a2/history_{date_start}{date_end}/lang:EN/units:english/bestfct:1/v:2.0/q/UUWW.json'
+    params['parforget'] = {'showObs' : '0',
+                'ttl' : '120'}    
+    
+    return params
 
-with open('out/data.json', 'w') as write_file:
-    json.dump(r.json(), write_file, indent=2, ensure_ascii=False)
+def write_json(answ_json):
+    # Сохраняем json
+    dir_out = 'out'
+    if not os.path.exists(dir_out):
+        os.makedirs(dir_out)
+
+    with open('out/data.json', 'w') as write_file:
+        json.dump(answ_json, write_file, indent=2, ensure_ascii=False)
+
+def do_request(session, params):
+    try:
+        r = session.get(params['URL'], headers=params['headers'], params=params['parforget'])
+    except HTTPError as e:
+        print(e)
+    else:
+        pass
+    write_json(r.json())
+
+def main():
+    session = do_session()
+    params = get_params_for_req()
+    do_request(session, params)
+
+if __name__ == "__main__":
+    main()
