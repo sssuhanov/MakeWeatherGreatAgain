b0c9904001657ef688fadc4db1996c39b673e7ba
Sergey Sukhanov <sssuhanov@gmail.com>
2019-08-30 13:34:26 +0300

    add sql uploader v0.05

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import json
 import os
 import time
+import sqlalchemy as db
 
 
 def do_session():
@@ -57,7 +58,66 @@ def get_weather():
         print(f'end {year}')
         time.sleep(10)
 
+def read_json(file_adr):
+    with open(f'out/{file_adr}', 'r') as f:
+        data = json.load(f)
+    return data
+
+def upload_sql(all_days_weather_list):
+    # from sqlalchemy import create_engine
+    engine = db.create_engine('postgresql://postgres:kilo98ui@localhost/weather')
+    connection = engine.connect()
+    metadata = db.MetaData()
+    observ = db.Table('observ', metadata, autoload=True, autoload_with=engine)
+
+    #Inserting many records at ones
+    query = db.insert(observ) 
+    ResultProxy = connection.execute(query, all_days_weather_list)
+    
+
+def read_weather_days(files_list):
+    for json_file in files_list:
+        # data_weather_day = read_weather_params(json_file)
+        weather = read_json(json_file)
+
+        all_days_weather_list = []
+
+        for weather_day in weather['history']['days']:
+            # Достаем из файла необходимые данные в удобном формате
+            data_weather_day = {}
+            # Дату
+            year = weather_day['summary']['date']['year']
+            month = weather_day['summary']['date']['month']
+            day = weather_day['summary']['date']['day']
+            data_weather_day['date'] = f'{year}-{month}-{day}'
+
+            # показатели погоды
+            list_of_params = ['temperature', 'dewpoint', 'pressure', 'wind_speed',
+                                'visibility', 'max_temperature', 'min_temperature',
+                                'max_dewpoint', 'min_dewpoint', 'max_humidity',
+                                'min_humidity', 'max_wind_speed', 'min_wind_speed',
+                                'max_pressure', 'min_pressure', 'max_visibility',
+                                'min_visibility', 'rain', 'snow', 'fog', 'hail',
+                                'thunder', 'tornado', 'snowfall']
+            
+            for param in list_of_params:
+                data_weather_day[param] = weather_day['summary'][param]
+            
+            all_days_weather_list.append(data_weather_day)
+
+        # Подключаемся к SQL и записываем данные
+        if len(all_days_weather_list) > 0:
+            upload_sql(all_days_weather_list)
+            print(f'{json_file} в базу записан')
+        else:
+            print(f'{json_file} пустой, пропущен')
+
+
 def push_weather():
+    # Получить список файлов в папке out
+    files_list = os.listdir('out')
+    # Считываем JSON файл
+    read_weather_days(files_list)
     pass
 
 def choiser():
